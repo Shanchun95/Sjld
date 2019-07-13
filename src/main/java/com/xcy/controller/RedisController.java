cedf658a484585c8b92c975f11cdab537b6c2809
shanchun02 <1446533395@qq.com>
2019-07-13 11:47:16 +0800

    添加过redis的三级联动

--- src/main/java/com/xcy/controller/RedisController.java
+++ src/main/java/com/xcy/controller/RedisController.java
@@ -33,7 +33,7 @@ public class RedisController {
             //将数据转换为json，放入到redis数据库中
             String jsonData = JsonUtils.objectToJson(Slist);
             jedisClient.set("PROVINCEALL", jsonData);
-            System.out.println("此数据是从数据库中获取" + jsonData);
+            System.out.println("此数据是从数据库中获取; provinceall:" + jsonData);
         }
 
 
@@ -42,18 +42,39 @@ public class RedisController {
     @RequestMapping("/getCity")
     @ResponseBody
     public List<Area> getCity(int id){
-        Boolean isExists = jedisClient.exists("CITY");
+        Boolean isExists = jedisClient.exists("CITY"+id);
+        List<Area> Xlist = null;
         if(isExists){
-            String cityList = jedisClient.get("CITY");
+            String jsonData02 = jedisClient.get("CITY"+id);
+            JsonUtils.jsonToList(jsonData02,Area.class);
+            System.out.println("从缓存中获取："+ jsonData02);
+        }else{
+            Xlist = areaService.getCity(id);
+            //将数据转换成json数据，并放入redis数据库中
+            String jsonData02 = JsonUtils.objectToJson(Xlist);
+            jedisClient.set("CITY"+id,jsonData02);
+            System.out.println("从MySQL数据库中获取数据; city:" + jsonData02);
         }
-        List<Area> list = areaService.getCity(id);
-        return list;
+
+        return Xlist;
     }
     @RequestMapping("/getCountry")
     @ResponseBody
     public List<Area> getCountry(int id){
-        List<Area> list = areaService.getCountry(id);
-        return list;
+        Boolean isExists = jedisClient.exists("COUNTRY"+id);
+        List<Area> QList = null;
+        if (isExists){
+            String jsonData03 = jedisClient.get("COUNTRY"+id);
+            QList = JsonUtils.jsonToList(jsonData03,Area.class);
+            System.out.println("从redis数据库中获取； Country:" + jsonData03);
+        }else{
+            QList = areaService.getCountry(id);
+            String jsonData03 = JsonUtils.objectToJson(QList);
+            jedisClient.set("COUNTRY"+id,jsonData03);
+            System.out.println("从MySQL数据库中获取; Country:" + jsonData03);
+        }
+
+        return QList;
     }
 
 }
